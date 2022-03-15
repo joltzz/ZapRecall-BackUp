5e8a82f7bff2490a752c359422adc82f06796aab
joltzz <guijsilva02@gmail.com>
2022-03-15 15:26:19 -0300

    iniciando o projeto com arquivos basicos

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import reactDom from "react-dom";
+
+function App(){
+    return(
+        <>
+        </>
+    )
+}
+//
+//codigo
+//
+
+let root=document.querySelector(".root");
+reactDom.render(<App />,root);
